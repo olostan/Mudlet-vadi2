6fc9e545be9406d00eb538862f26eeb91fc36480
Vadim Peretokin <vperetokin@gmail.com>
2011-05-17 20:08:03 +1000

    Added an option of using your own map when starting the mapper
    Signed-off-by: Vadim Peretokin <vperetokin@gmail.com>

--- src/TMap.cpp
+++ src/TMap.cpp
@@ -1049,12 +1049,16 @@ bool TMap::restore(QString location)
             || mpHost->mUrl == "midkemiaonline.com"
             || mpHost->mUrl == "lusternia.com" )
         {
-            msgBox.setText("No map found. Going to download the map ...");
+            msgBox.setText("No map found. Would you like to download the map or start your own?");
+            QPushButton *yesButton = msgBox.addButton("Download the map", QMessageBox::ActionRole);
+            QPushButton *noButton = msgBox.addButton("Start my own", QMessageBox::ActionRole);
             msgBox.exec();
             init( mpHost );
-            qDebug()<<"--trace before map download";
-            mpMapper->downloadMap();
-            qDebug()<<"--trace after map download";
+            if (msgBox.clickedButton() == yesButton) {
+                qDebug()<<"--trace before map download";
+                mpMapper->downloadMap();
+                qDebug()<<"--trace after map download";
+            }
         }
         else
         {
